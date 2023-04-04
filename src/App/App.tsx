169aaa4095161c7b159c70dda62195ccc9a3c91f
Paul Delifer <paul.delifer@ikmail.com>
2023-04-04 10:23:05 +0200

    test

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -18,7 +18,6 @@ function App() {
             <Controller></Controller>
             <br/>
         </div>
-
     );
 }
 
--- src/App/store.ts
+++ src/App/store.ts
@@ -1,9 +1,13 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import {createEpicMiddleware} from "redux-observable";
+const epicMiddleware = createEpicMiddleware();
+
 export const store = configureStore({
     reducer: {
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(epicMiddleware)
 })
-
+epicMiddleware.run(rootEpic);
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
